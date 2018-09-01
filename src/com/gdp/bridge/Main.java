4d5cf184efbb00fcb9187fa6c61da24506def999
Hurf <huruifeng1202@163.com>
2018-09-01 13:04:13 +0800

    学习桥接模式

---
+++ src/com/gdp/bridge/Main.java
@@ -0,0 +1,13 @@
+package com.gdp.bridge;
+
+public class Main {
+	public static void main(String[] args) {
+		Display d1 = new Display(new StringDisplayImpl("Hello, China."));
+		Display d2 = new CountDisplay(new StringDisplayImpl("Hello, World."));
+		CountDisplay d3 = new CountDisplay(new StringDisplayImpl("Hello, Universe."));
+		d1.display();
+		d2.display();
+		d3.display();
+		d3.multiDisplay(5);
+	}
+}	
---
+++ src/com/gdp/bridge/StringDisplayImpl.java
@@ -0,0 +1,35 @@
+package com.gdp.bridge;
+
+public class StringDisplayImpl extends DisplayImpl {
+	
+	private String string;
+	private int width;
+	
+	public StringDisplayImpl(String string) {
+		this.string = string;
+		this.width = string.getBytes().length;
+	}
+
+	@Override
+	public void rawOpen() {
+		printLine();
+	}
+
+	@Override
+	public void rawPrint() {
+		System.out.println("|" + string + "|");
+	}
+
+	@Override
+	public void rawClose() {
+		printLine();
+	}
+
+	private void printLine() {
+		System.out.print("+");
+		for (int i = 0; i < width; i++) {
+			System.out.print("-");
+		}
+		System.out.println("+");
+	}
+}
